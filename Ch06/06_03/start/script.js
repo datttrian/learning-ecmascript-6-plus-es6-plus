9b0948535cdb2f44ae71c2aa600c2cf9b7d17cd1
datttrian <datttrian@gmail.com>
2023-08-26 03:34:01 +0000

    Returning promises with fetch

--- Ch06/06_03/start/script.js
+++ Ch06/06_03/start/script.js
@@ -1,23 +1,10 @@
-const spacePeople = () => {
-  return new Promise((resolves, rejects) => {
-    const api =
-      "http://api.open-notify.org/astros.json";
-    const request = new XMLHttpRequest();
-    request.open("GET", api);
-    request.onload = () => {
-      if (request.status === 200) {
-        resolves(JSON.parse(request.response));
-      } else {
-        rejects(Error(request.statusText));
-      }
-    };
-    request.onerror = (err) => rejects(err);
-    request.send();
-  });
-};
+const getSpacePeople = () =>
+  fetch("http://api.open-notify.org/astros.json").then((res) => res.json());
 
-spacePeople().then(
-  (spaceData) => console.log(spaceData),
-  (err) =>
-    console.error(new Error("Can't load people"))
-);
+const spaceNames = () =>
+  getSpacePeople()
+    .then((json) => json.people)
+    .then((people) => people.map((p) => p.name))
+    .then((names) => names.join(", "));
+
+spaceNames().then(console.log);
