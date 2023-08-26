8c4be285114389f8041df1cdbd9262d8832cd0ce
datttrian <datttrian@gmail.com>
2023-08-26 01:30:49 +0000

    Searching strings

---
+++ Ch02/02_04/start/script.js
@@ -0,0 +1,6 @@
+const planet = "Earth";
+// console.log(planet.includes("th"));
+// console.log(planet.includes("Earth1"));
+// console.log(planet.includes("art"));
+
+console.log(planet.search("art"));
