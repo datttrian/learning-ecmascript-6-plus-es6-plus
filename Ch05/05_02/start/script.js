e8699625f23db15e03c85d7ce1a72945fd7874ca
datttrian <datttrian@gmail.com>
2023-08-26 02:45:17 +0000

    Setting default function parameters

---
+++ Ch05/05_02/start/script.js
@@ -0,0 +1,13 @@
+add = (x = 5, y = 6) => {
+  console.log(x + y);
+};
+
+add(1, 2);
+
+function haveFun(activityName = "hiking", time = 3) {
+  console.log(`Today I will go ${activityName}
+  for ${time} hours`);
+}
+
+haveFun("biking", 2.5);
+haveFun("biking");
