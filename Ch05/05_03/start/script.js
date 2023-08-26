9d070b323dd8adf1ae2ce7e9f32d8ee3092b71de
datttrian <datttrian@gmail.com>
2023-08-26 02:51:37 +0000

    Writing arrow functions

---
+++ Ch05/05_03/start/script.js
@@ -0,0 +1,9 @@
+// =>
+
+const studentList = (students) => console.log(students);
+
+studentList(["A", "B", "C"]);
+
+const list = ["apple", "banana", "cherry"];
+
+list.map((item) => console.log(item));
