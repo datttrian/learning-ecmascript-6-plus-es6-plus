916d0fb5e18c459c7a4726f937059ced3a8a76f9
datttrian <datttrian@gmail.com>
2023-08-26 02:11:13 +0000

    Introducing classes

---
+++ Ch04/04_05/start/script.js
@@ -0,0 +1,16 @@
+class Vehicle {
+  constructor(description, wheels) {
+    this.description = description;
+    this.wheels = wheels;
+  }
+
+  describeYourself() {
+    console.log(`I am a ${this.description}
+    with ${this.wheels} wheels.`);
+  }
+}
+
+const coolSkiVan = new Vehicle("cool ski van", 4);
+
+console.log(coolSkiVan);
+coolSkiVan.describeYourself();
